5beb8df008dd1c39d5b819c4b9d00f336850f41b
vagrant <vagrant@homestead>
2020-09-05 00:32:19 +0000

    記事一覧

--- resources/views/front/article/list.blade.php
+++ resources/views/front/article/list.blade.php
@@ -14,14 +14,17 @@
     @foreach($Articles as $Article)
     <div class="kiji">
     <a href="{{route('article')}}/{{$Article->id}}" style="width:100%;">
-		<p class="kiji_title">{{$Article->title}}</p>
-		<p class="kiji_day">
-            {{$Article->release_at->format('Y年m月d日')}}　{{$Article->category_name}}
-            <img src="{{route("index")}}/images/file.png">
-        </p>
-        <p class="kiji_gazo"><img src="{{route("index")}}/images/gazo.png"></p>
-		<p class="kiji_in">ああああああああああああああああああああああ...[続きを読む]</p>
-		</a>
+	<p class="kiji_title">{{$Article->title}}</p>
+	<p class="kiji_day">
+        {{$Article->release_at->format('Y年m月d日')}}　{{$Article->category_name}}
+        <img src="{{route("index")}}/images/file.png">
+    </p>
+    <p class="kiji_gazo"><img src="{{route("index")}}/images/gazo.png"></p>
+    <div class='article_lead'>
+	<p class="kiji_in">{!!$Article->main!!}</p>
+    <span class="article_open">[続きを読む]</span>
+    </div>
+	</a>
 	</div>
     @endforeach
     @else
